57c482d1ad160d120c6399213e82a37ef5d1eb23
Дмитрий Чернаков <ChernakovDMT@yandex.ru>
2022-07-20 17:58:28 +0700

    changed the language of the phrase to Program.cs

--- TrainingProjectOnGItHub/Program.cs
+++ TrainingProjectOnGItHub/Program.cs
@@ -10,7 +10,7 @@ namespace TrainingProjectOnGItHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Привет, Мир!");
         }
     }
 }
